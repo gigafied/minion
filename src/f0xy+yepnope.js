ff4ee0d50c864ce4887b5937d59b5374b8aa6a8e
Taka Kojima <taka.kojima@ff0000.com>
2011-11-18 00:43:54 -0800

    updated f0xy documentation comments

--- src/f0xy+yepnope.js
+++ src/f0xy+yepnope.js
@@ -582,7 +582,7 @@ var docElement            = doc.documentElement,
 		Global Static f0xy Class with static methods.
 		@static
 		@class
- */ 
+*/ 
 
 var f0xy = (function(){
 
@@ -608,33 +608,125 @@ var f0xy = (function(){
 	var _origWindowNS = {};
 
 	/**
+	* Used by f0xy.get() and f0xy.define(). 
+	* Get the namespace/Class, or creates it if it does not exist. Also optionally creates Objects in the specified namepsace.
+	*
+	* @public
+	* @param			{String|Object}	identifier			The fully qualified namespace.
+	* @param			{Boolean}			autoCreate			Whether or not to create a blank object if the namespace does not yet exist.
+	* @param			{Object}				[classes]			An object of class definitions which will be added to the namespace.
+	* @returns		{Object}										The object that represents the fully qualified namespace passed in as the first argument.
+	* @ignore
+	*/
+
+	_namespace = function(identifier, autoCreate, classes){
+		classes = classes || false;
+		var ns = window;
+
+		if(identifier != '' && typeof identifier !== "object") {
+			var parts = identifier.split(_separator);
+			for (var i = 0; i < parts.length; i++) {
+				if(!ns[parts[i]]){
+					if(autoCreate){
+						ns[parts[i]] = {};
+					}
+					else{
+						return false;
+					}
+				}
+				ns = ns[parts[i]];
+			}
+		}
+		else if(typeof identifier === "object"){ns = identifier;}
+
+		if(classes !== false){
+			
+			for(var className in classes){
+				if(classes[className].superClassIdentifier){
+					if(!classes.require){classes.require = [];}
+					classes.require.push(classes[className].superClassIdentifier);
+					if(_extendQueue.indexOf(identifier + _separator + className) === -1){
+						_extendQueue.push(identifier + _separator + className);
+					}
+				}
+			}
+
+			if("require" in classes && classes.require.length > 0){
+				
+				for(var className in classes){
+
+					if(className !== "require"){
+
+						var addTo = (_f0xy.isClass(classes[className])) ? classes[className].prototype : classes[className];
+						
+						addTo.dependencies = classes.require;
+						addTo.nsID = identifier;
+						addTo.ns = ns;
+						addTo.className = className;
+
+						ns[className] = classes[className];
+					}
+				}
+
+				_f0xy.require(classes.require);
+			}
+		}
+
+		return ns;
+	}	
+
+	/**
 	* @exports _f0xy as f0xy 
 	* @class
 	*/
 	var _f0xy = {};
 
 	/**
-	 * Configure f0xy. Not necessary, unless you want to change the default class path or Namespace separator.
-	 * 
-	 * @public
-	 * @param		 {String}			[separator="."]		Namespace separator
-	 * @param		 {String}			[class_path="js/"]	The root path of all your classes. Can be absolute or relative.
-	 */
+	* Configure f0xy. Call to update the base class path, or to change the default the separator (".").
+	* 
+	* @public
+	* @param		 {String}			[separator="."]		Namespace separator
+	* @param		 {String}			[class_path="js/"]	The root path of all your classes. Can be absolute or relative.
+	*/
 
-	_f0xy.configure = function(separator, class_path){
-		_separator = separator || _separator;
+	_f0xy.configure = function(class_path, separator){
 		_class_path = class_path || _class_path;
+		_separator = separator || _separator;
 	}
 
 	/**
-	 * Gets the URL for a given identifier.
-	 *
-	 * @public
-	 * @param		 {String}			identifier			The namespace identifier
-	 * @returns		 {String}									The URL of the package/class.
-	 */
+	* Gets the object by it's fully qualified identifier.
+	*
+	* @public
+	* @param			{String}				identifier			The identifier to get
+	* @returns		{Object|Boolean}							The object that represents the identifier or False if it has not yet been defined.
+	*/
 
-	_f0xy.getIdentifierURL = function(identifier) {
+	_foxy.get = function(identifier){
+		return _namespace(identifier, false);
+	}
+
+	/**
+	* Defines Classes under the given namespace.
+	*
+	* @public
+	* @param			{String}				identifier			The namespace to define the Classes under.
+	* @param			{Object}				[classes]			An object of class definitions which will be added to the namespace
+	* @returns		{Object}										The object that represents the namespace passed in as the first argument.
+	*/
+	_foxy.define = function(identifier, classes){
+		return _namespace(identifier, true, classes);
+	}
+
+	/**
+	* Gets the URL for a given identifier.
+	*
+	* @public
+	* @param		 	{String}			identifier			The fully qualified name to look up.
+	* @returns		{String}									The URL of the file that maps to the fully qualified name.
+	*/
+
+	_f0xy.getURL = function(identifier) {
 		
 		if(_classMappings[identifier]){
 			return _classMappings[identifier];
@@ -644,130 +736,41 @@ var f0xy = (function(){
 	}
 
 	/**
-	 * Checks to see whether the given identifier or Class is a f0xy class. (Checks for .isClass)<br>
-	 * NOTE: Classes that have not yet loaded all of their dependencies, will return FALSE for this check.
-	 *
-	 * @public
-	 * @param		{String|Object}	identifier			The namespace identifier, the Class or an instance of the class.
-	 * @returns		{Boolean}									Whether or not this is a Class.
-	 */
+	* Checks to see whether the given fully qualified name or Object is a f0xy class. (Checks for .isClass)<br>
+	* NOTE: Classes that have not yet loaded all of their dependencies, will return FALSE for this check.
+	*
+	* @public
+	* @param			{String|Object}		identifier			The fully qualfied class name, or an Object.
+	* @returns		{Boolean}										Whether or not this is a Class.
+	*/
 
 	_f0xy.isClass = function(identifier){
 		
-		if(typeof identifier === "string"){
-			var parts = identifier.split(_separator);
-			var identifierClass = window;
-			
-			for (var j = 0; j < parts.length; j++){
-				if(!identifierClass[parts[j]]){
-					return false;
-				}
-				identifierClass = identifierClass[parts[j]];
-			}
-		}
-
-		else{
-			identifierClass = identifier;
-		}
+		identifier = _namespace(identifier, false);
 		
-		if(typeof identifierClass === "object" || typeof identifierClass === "function"){
-			return identifierClass.isClass;
+		if(typeof identifier === "object" || typeof identifier === "function"){
+			return identifier.isClass;
 		}
 
 		return false;
 	}
 
-	/**
-	 * Gets the namespace/Class, or creates it if it does not exist. Also optionally creates Objects in the specified namepsace.
-	 *
-	 * @public
-	 * @param		{String}				namespace			The namespace/package as a {String}
-	 * @param		{Object}				[classes]			An object of class definitions which will be added to the namespace
-	 * @returns		{Object}										The object that represents the namespace passed in as the first argument.
-	 */
-
-	_f0xy.namespace = function(namespace, classes){
-		classes = classes || false;
-		var ns = window;
-
-		if(namespace != '') {
-			var parts = namespace.split(_separator);
-			for (var i = 0; i < parts.length; i++) {
-				if (!ns[parts[i]]){
-					ns[parts[i]] = {};
-				}
-				ns = ns[parts[i]];
-			}
-		}
-
-		if(classes !== false){
-			
-			for(var className in classes){
-				if(classes[className].superClassIdentifier){
-					if(!classes.requires){classes.requires = [];}
-					classes.requires.push(classes[className].superClassIdentifier);
-					if(_extendQueue.indexOf(namespace + _separator + className) === -1){
-						_extendQueue.push(namespace + _separator + className);
-					}
-				}
-			}
-
-			if("requires" in classes){
-				
-				for(var className in classes){
-					if(className !== "requires"){
-						if(!_f0xy.isClass(classes[className])){
-							classes[className].dependencies = classes.requires;
-							classes[className].nsID = namespace;
-							classes[className].ns = ns;
-							classes[className].className = className;
-						}
-						else{
-							classes[className].prototype.dependencies = classes.requires;
-							classes[className].prototype.nsID = namespace;
-							classes[className].prototype.ns = ns;
-							classes[className].prototype.className = className;
-						}
-
-						ns[className] = classes[className];
-					}
-				}
-				_f0xy.require(classes.requires, function(){});
-			}
-		}
-
-		return ns;
-	}
 
 	/**
-	 * extends a given class asynchronously.
-	 *
-	 * @public
-	 * @param		 {String}			identifier			The fully qualified class name of the class you want to extend.
-	 * @param		 {Object}			obj					A Class Object
-	 * @returns		 {Object}									The extended Class, or, if the Class to be extended has yet to be loaded in, the original Object with a few more properties.
-	 */ 
+	* Extends a given class asynchronously.
+	*
+	* @public
+	* @param			{String}			identifier			The fully qualified name of the Class you want to extend.
+	* @param			{Object}			obj					A new Class Object
+	* @returns		{Object}									The extended Class, or, if still waiting on dependencies, the original Object with a few more properties for internal f0xy use.
+	*/ 
 
 	_f0xy.extend = function(identifier, obj){
-	 
-		// Let's check to see if the Class is already defined...
-		var parts = identifier.split(_separator);
-		var identifierClass = window;
-		
-		for (var j = 0; j < parts.length; j++){
-			
-			if(!identifierClass[parts[j]]){
-				break;
-			}
-
-			identifierClass = identifierClass[parts[j]];
-		}
 		
 		// If the Class exists and is a f0xy class, then return the extended object.
-		if(identifierClass.isClass){
+		if(_foxy.isClass(identifier)){
 			obj = identifierClass.extend(obj);			
 		}
-
 		else{
 			obj.superClassIdentifier = identifier;
 		}
@@ -778,53 +781,46 @@ var f0xy = (function(){
 	}
 
 	/**
-	 * Imports properties from the specified namespace to the global space (ie. under window)
-	 *
-	 * The identifier {String} can contain the * wildcard character as its last segment (eg: com.test.*) 
-	 * which will import all properties from the namespace.
-	 * 
-	 * If not, the targeted namespace will be imported (ie. if com.test is imported, the test object 
-	 * will now be global).
-	 * 
-	 * Only use f0xy.use() if you know the Class/package has already been defined (loaded).
-	 * Because f0xy.use() imports objects/namespaces into the global namespace (window),
-	 * it is highly recommended that you call f0xy.unuse() to clean up afterwards.
-	 *
-	 * f0xy.unuse() is automatically called after every Class method.
-	 *
-	 * @public
-	 * @param	 {String|Array}		identifier		The namespace as a String or an Array of namespaces
-	 */
+	* Imports properties from the specified namespace to the global space (ie. under window)
+	* This is only meant to be used as a utility, and for temporary purposes. Please clean up with f0xy.unuse()
+	* You are responsible for not polluting the global namespace.
+	*
+	* By calling f0xy.use("com.test.Example"), you will be able to refer to com.test.Example as just Example.
+	* By calling f0xy.use("com.test"), you will be able to refer to com.test.Example as just test.Example.
+	* 
+	* Identifiers can contain the* wildcard character as its last segment (eg: com.test.*) 
+	* which will import all Classes under the given namespace.
+	*
+	* @see 		foxy.unuse
+	* @public
+	* @param	 	{String|Array}		identifiers		The fully qualfiied name(s) to import into the global namespace.
+	*/
 	 
-	_f0xy.use = function(identifier){
+	_f0xy.use = function(identifiers){
 		
-		f0xy.unuse(identifier);
+		f0xy.unuse(identifiers);
 
-		var identifiers;
-
-		if(typeof(identifier) !== 'object' && !identifier.sort){
-			identifiers = new Array(identifier);
-		}
-		else{
-			identifiers = identifier;
+		if(typeof(identifiers) !== 'object' && !identifiers.sort){
+			identifiers = new Array(identifiers);
 		}
 
 		for (var i = 0; i < identifiers.length; i++) {
-			identifier = identifiers[i];	
+			
+			var identifier = identifiers[i];	
 			
 			var parts = identifier.split(_separator);
 			var target = parts.pop();
-			var ns = _f0xy.namespace(parts.join(_separator));
+			var ns = _f0xy.get(parts.join(_separator), false);
 			
 			if (target === '*') {
-				// imports all objects from the identifier, can't use include() in that case
-				for (var objectName in ns) {
+				// imports all Classes/namespaces under the given namespace
+				for(var objectName in ns){
 					_origWindowNS[objectName] = (window[objectName]) ? window[objectName] : null;
 					window[objectName] = ns[objectName];
 				}
 			}
 			else{
-				// imports only one object
+				// imports only the specified Class/namespace
 				if(ns[target]){
 					_origWindowNS[target] = (window[target]) ? window[target] : null;
 					window[target] = ns[target];
@@ -835,69 +831,39 @@ var f0xy = (function(){
 
 
 	/**
-	 * The opposite of f0xy.use() (who would'a thunk it!!)
-	 *
-	 * This method is automatically called after every Class method.
-	 *
-	 * @public
-	 * @param	 {String|Array}		identifier		The namespace as a String or an Array of namespaces
-	 */
+	* Clears all temporary global namespacing mappings created by f0xy.use(). This method has no arguments, it clears all
+	* temporary namespaces.
+	* 
+	* @see f0xy.use
+	*
+	* @public
+	*/
 
-	_f0xy.unuse = function(identifier){
+	_f0xy.unuse = function(){
 
-		identifier = identifier || "*";
-		var identifiers;
-
-		if(typeof(identifier) !== 'object' && !identifier.sort){
-			identifiers = new Array(identifier);
-		}
-		else{
-			identifiers = identifier;
-		}
-
-		for (var i = 0; i < identifiers.length; i++) {
-			
-			identifier = identifiers[i];
-
-			var parts = identifier.split(_separator);
-			var target = parts.pop();
-
-			if(target === '*'){
-				for(var prop in _origWindowNS){
-					window[prop] = _origWindowNS[prop];
-					if(window[prop] === null){
-						delete window[prop];
-					}
-				}
-			}
-			else{
-				// imports only one object
-				if(_origWindowNS[target]){
-					window[target] = _origWindowNS[target];
-					if(window[target] === null){
-						delete window[target];
-					}
-				}
+		for(var prop in _origWindowNS){
+			window[prop] = _origWindowNS[prop];
+			if(window[prop] === null){
+				delete window[prop];
 			}
 		}
 	}
 
-
 	/**
-	 * Tells f0xy that x file provides these Class definitions.
-	 * Where x file = filePath and classes is an aray of the Classes that xFile defines.
-	 * Useful in cases where you group specific things into minfiied js files.
-	 *
-	 * f0xy.provides can load the file right away, by passing doLoad as true, and a callback function.
-	 * Otherwise, it just maps the classes to the filePath for any subsequent calls to f0xy.require() which might refer
-	 * to said classes.
-	 *
-	 * @public
-	 * @param	 {String}					filePath			The path to the JS file.
-	 * @param	 {String|Array}			identifier		The namespace as a {String} or an {Array} of namespaces
-	 */
+	* Tells f0xy that filePath provides the class definitions for these classes.
+	* Useful in cases where you group specific things into minfiied js files.
+	*
+	* f0xy.provides can load the file right away, by passing doLoad as true, and a callback function.
+	* Otherwise, it just maps the classes to the specified filePath for any subsequent calls to f0xy.require()
+	*
+	* @public
+	* @param		{String}					file					The path of a JS file.
+	* @param	 	{String|Array}			classes				Fully qualfiied name(s) of class(es)
+	* @param		{Boolean}				[doLoad=false]		Whether or not to subsequently call f0xy.require()
+	* @param 	{Function}				[callback=null]	If doLoad=true, the callback function to call once the file has been loaded.
+	*/
 
-	_f0xy.provides = function(filePath, classes, doLoad, callback){
+	_f0xy.provides = function(file, classes, doLoad, callback){
 
 		// If classes is a String, create an array
 		if(typeof(classes) !== 'object' && !classes.sort){
@@ -905,7 +871,7 @@ var f0xy = (function(){
 		}
 
 		for(var i = 0; i < classes.length; i ++){
-			_classMappings[classes[i]] = filePath;
+			_classMappings[classes[i]] = file;
 		}
 
 		if(doLoad){
@@ -915,12 +881,13 @@ var f0xy = (function(){
 
 
 	/**
-	 * Imports the packages/classes for a given identifier. Loads them if neccessary and includes them in the global namespace.
-	 *
-	 * @public
-	 * @param	 {String|Array}		classes					The class(es) to load/include.
-	 * @param	 {Function}				completeFunc			The function to call once all classes (and their dependencies) have been included/loaded.
-	 */
+	* Asyncrhonously loads in js files for the classes specified.
+	* If the classes have already been loaded, or are already defined, the callback function is invoked immediately.
+	*
+	* @public
+	* @param	 {String|Array}		classes					The fully qualified names of the class(es) to load.
+	* @param	 {Function}				completeFunc			The function to call once all classes (and their dependencies) have been loaded.
+	*/
 
 	_f0xy.require = function(classes, completeFunc){
 
@@ -933,7 +900,7 @@ var f0xy = (function(){
 
 			if((_loadedClasses.indexOf(identifier) === -1) && !_f0xy.isClass(identifier)){	
 				_loadedClasses.push(identifier);
-				classFiles.push(_f0xy.getIdentifierURL(identifier));
+				classFiles.push(_f0xy.toURL(identifier));
 			}
 		}
 
@@ -1017,26 +984,20 @@ var f0xy = (function(){
 		}
 	}
 
-	return _f0xy;
+	/**
+	* The f0xy Base Class
+	* Simple JavaScript Inheritance
+	*
+	* Taken from: http://ejohn.org/blog/simple-javascript-inheritance/
+	* MIT Licensed.
+	*
+	* @class
+	* @private
+	*/ 
 
-})();
-
-/**
-	The f0xy Base Class
-	Simple JavaScript Inheritance
-
-	Taken from: http://ejohn.org/blog/simple-javascript-inheritance/
-	MIT Licensed.
-
-	@class
-	@private
- */ 
-
-f0xy.namespace("org.f0xy", {
-	
 	// ## f0xy Base Class.
-	// org.f0xy.Class is the ONLY Class to extend this directly, do not directly extend this Class.
-	$$__BaseClass__$$ : (function(doInitialize){
+	// f0xy.Class is the ONLY Class to extend this directly, do not directly extend this Class.
+	_foxy.$$__BaseClass__$$ = (function(doInitialize){
 		
 		// The base Class implementation (does nothing)
 		var _BaseClass = function(){};
@@ -1095,14 +1056,17 @@ f0xy.namespace("org.f0xy", {
 		};
 		
 		return _BaseClass;
-	})()
-});
+	})();	
 
-f0xy.namespace("org.f0xy", {
+	return _f0xy;
 
-	/** @lends org.f0xy.Class# */
+})();
 
-	Class : f0xy.extend("org.f0xy.$$__BaseClass__$$", {
+f0xy.namespace("f0xy", {
+
+	/** @lends f0xy.Class#*/
+
+	Class : f0xy.extend("f0xy.$$__BaseClass__$$", {
 
 		/** 
 		*
@@ -1116,12 +1080,12 @@ f0xy.namespace("org.f0xy", {
 		},
 
 		/** 
-		* Imports all the dependencies (determined by what is in the "requires" array and what Class this Class extends) to the global namespace temporarily.
+		* Imports all the dependencies (determined by what is in the "require" array and what Class this Class extends) to the global namespace temporarily.
 		* Basically, it just does: <code>f0xy.use(this.dependencies);</code>
 		*
 		* @see f0xy.use
 		*/
-		import_dependencies : function(){
+		use_dependencies : function(){
 			if(this.dependencies){
 				f0xy.use(this.dependencies)
 			}
