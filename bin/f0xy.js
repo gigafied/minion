5c29aaa80af56978b7fd879e9f1c09538e45fbd5
Taka Kojima <taka.kojima@ff0000.com>
2011-11-22 03:54:22 -0800

    updated comments

--- bin/f0xy.js
+++ bin/f0xy.js
@@ -5,47 +5,40 @@
  * (c) 2011, Taka Kojima
  * Licensed under the MIT License
  *
- * Date: Tue Nov 22 03:08:37 2011 -0800
+ * Date: Tue Nov 22 03:09:15 2011 -0800
  */
  
 /**
 
 @fileOverview
 
-	<h4>f0xy - AMD inspired Classy JavaScript</h4>
+<h4>f0xy - AMD inspired Classy JavaScript</h4>
 
-	<p>Permission is hereby granted, free of charge, to any person obtaining a copy
-	of this software and associated documentation files (the "Software"), to deal
-	in the Software without restriction, including without limitation the rights
-	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-	copies of the Software, and to permit persons to whom the Software is
-	furnished to do so, subject to the following conditions:</p>
+<p>Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:</p>
 
-	<p>The above copyright notice and this permission notice shall be included in
-	all copies or substantial portions of the Software.</p>
+<p>The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.</p>
 
-	<p>THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-	THE SOFTWARE.</p>
-	
-	Inspired by NamespaceJS: https://github.com/maximebf/Namespace.js
-
-	@ author Taka Kojima (taka@gigafied.com)
-	@ version 1.0
+<p>THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.</p>
 */
 
 
 /**
-		Global Static f0xy Class with static methods.
-		@static
-		@class
+*	Global Static f0xy Class with static methods.
+*	
+*	@namespace
 */
-
-/** @namespace */
 
 var f0xy = (function(root){
 
